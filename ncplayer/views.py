448cfd00ad7957c4cb9c646361f662e7c0fd51d0
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-17 23:48:23 -0500

    mvc

--- ncplayer/views.py
+++ ncplayer/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'ncplayer/post_list.html',
+    return render(request, 'post_list.html',
                   {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
