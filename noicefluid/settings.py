ed2d9072dc55e64bd25ae8a25159acabc750a707
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-22 00:37:49 -0500

    whitenoise

--- noicefluid/settings.py
+++ noicefluid/settings.py
@@ -83,7 +83,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 
 HOST_URL = 'https://fluidsv.herokuapp.com'
--- noicefluid/wsgi.py
+++ noicefluid/wsgi.py
@@ -1,7 +1,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'noicefluid.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justchat.settings')
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
