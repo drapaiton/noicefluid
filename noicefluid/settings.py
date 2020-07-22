0b7cd0368749c3447850f14e254df66c4520af17
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-21 22:17:56 -0500

    allow it

--- noicefluid/settings.py
+++ noicefluid/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.noicefluid.com', '.fluidsv.herokuapp.com']
 
 
 # Application definition
