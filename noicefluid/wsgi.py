79dc198faa7e2a1d5f7342672e3b84b779612bd6
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-22 00:43:19 -0500

    middlewares

--- noicefluid/wsgi.py
+++ noicefluid/wsgi.py
@@ -1,10 +1,6 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justchat.settings')
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
