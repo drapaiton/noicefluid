538e4178c71008d9899fd20606739281d0e78fff
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-22 01:22:22 -0500

    static

--- noicefluid/settings.py
+++ noicefluid/settings.py
@@ -79,6 +79,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
