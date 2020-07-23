ae1099fe21ce4cf39323379350357f6291451c87
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-23 18:02:58 -0500

    context fix

--- noicefluid/settings_production.py
+++ noicefluid/settings_production.py
@@ -1,5 +1,7 @@
 import dj_database_url
 from decouple import config
+from .settings import DATABASES
+
 
 DATABASES['default'].update(dj_database_url.config(
     conn_max_age=500, ssl_require=True))
