7cfe7b22ddc04d0c4db66ac6fcf39d2ad1a45999
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-23 21:48:19 -0500

    chat template

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'chat/index.html', {})
-
-
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+class ChatView(TemplateView):
+    template_name = 'chat/chat.html'
