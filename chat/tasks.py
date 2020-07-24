d000fe503ea09843adbe6b0a6ee1313a8941dc5e
Dra. Paiton <63127299+drapaiton@users.noreply.github.com>
2020-07-24 01:56:07 -0500

    n/a

--- chat/tasks.py
+++ chat/tasks.py
@@ -8,12 +8,10 @@ from django.core.cache import cache
 
 channel_layer = get_channel_layer()
 
-
 @shared_task
 def add(channel_name, x, y):
     message = '{}+{}={}'.format(x, y, int(x) + int(y))
-    async_to_sync(channel_layer.send)(
-        channel_name, {"type": "chat.message", "message": message})
+    async_to_sync(channel_layer.send)(channel_name, {"type": "chat.message", "message": message})
 
 
 @shared_task
@@ -31,5 +29,4 @@ def url_status(channel_name, url):
             status = 'Not available'
 
     message = f'{url} status is {status}'
-    async_to_sync(channel_layer.send)(
-        channel_name, {"type": "chat.message", "message": message})
+    async_to_sync(channel_layer.send)(channel_name, {"type": "chat.message", "message": message})
