2e65ea9339afca181126f33765a93352c0c4f8b6
drapaiton <drapaiton@gmail.com>
2020-08-07 21:05:39 +0000

    yt scraped

--- chat/tasks.py
+++ chat/tasks.py
@@ -1,6 +1,9 @@
-import time
-
+# Html Get
+import re
+from urllib import parse, request
 import requests
+import time
+# django
 from asgiref.sync import async_to_sync
 from celery import shared_task
 from channels.layers import get_channel_layer
@@ -8,10 +11,32 @@ from django.core.cache import cache
 
 channel_layer = get_channel_layer()
 
+
 @shared_task
-def add(channel_name, x, y):
+def sum(channel_name, x, y):
     message = '{}+{}={}'.format(x, y, int(x) + int(y))
-    async_to_sync(channel_layer.send)(channel_name, {"type": "chat.message", "message": message})
+    async_to_sync(channel_layer.send)(
+        channel_name, {"type": "chat.message", "message": message})
+
+
+@shared_task
+def add(channel_name: str, search: str):
+    # ---- handle petition ----
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen(
+        'http://www.youtube.com/results?' + query_string)
+    # print(html_content.read().decode())
+
+    search_results = re.findall(
+        "\/watch\?v=(.{11})", html_content.read().decode())
+
+    message = ""
+    for i in search_results:
+        message += ('https://www.youtube.com/watch?v=' + i+'\n')
+
+    # send to youtube api
+    async_to_sync(channel_layer.send)(
+        channel_name, {"type": "chat.message", "message": message})
 
 
 @shared_task
@@ -29,4 +54,5 @@ def url_status(channel_name, url):
             status = 'Not available'
 
     message = f'{url} status is {status}'
-    async_to_sync(channel_layer.send)(channel_name, {"type": "chat.message", "message": message})
+    async_to_sync(channel_layer.send)(
+        channel_name, {"type": "chat.message", "message": message})
