aa85fbd4c32596529205f65eda168217a1e8f773
drapaiton <drapaiton@gmail.com>
2020-08-13 21:40:16 +0000

    portfolio webpage redirect

--- lobby/urls.py
+++ lobby/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
+
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', lambda request: redirect("https://drapaiton.github.io/"), name='about'),
 ]
