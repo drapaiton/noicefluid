3b8fe986aa5048d5aca15d8864c9abf20bc454f6
drapaiton <drapaiton@gmail.com>
2020-08-14 00:47:55 +0000

    search button 😍

--- static/js/chat/domManipulation.js
+++ static/js/chat/domManipulation.js
@@ -1,37 +1,39 @@
-"use strict";
 // Note The not null assertion operator, will not perform any runtime checks, 
 // it just tells the compiler you have special information and you know DOM
 // will not be null at runtime.
 var wss_protocol = window.location.protocol == 'https:' ? 'wss://' : 'ws://';
 var chatSocket = new WebSocket(wss_protocol + window.location.host + '/ws/chat/');
 // TODO obtain the history
-// ChatSocket behaviour
 chatSocket.onopen = function (e) {
     document.querySelector('#chat-log').value +=
         "Welcome to the noicefluid chat.\nHere you can chat in realtime with all others.\n";
 };
-// Append messages
 chatSocket.onmessage = function (e) {
     var data = JSON.parse(e.data);
     var message = data['message'];
     document.querySelector('#chat-log').value +=
         data.message + '\n';
 };
-// onclose
 chatSocket.onclose = function (e) {
     document.querySelector('#chat-log').value +=
         'Socket closed unexpectedly, please reload the page.\n';
 };
-// Submit Button (enter key)
 document.querySelector('#chat-message-input').focus();
 document.querySelector('#chat-message-input').onkeyup = function (e) {
-    if (e.keyCode === 13) {
-        // enter, return
+    if (e.keyCode === 13) { // enter, return
         document.querySelector('#chat-message-submit').click();
     }
 };
-// Submit Button
+// SEND
 document.querySelector('#chat-message-submit').onclick = function (e) {
+    var messageInputDom = document.querySelector('#chat-message-input');
+    var message = messageInputDom.value;
+    document.querySelector('#chat-log').value +=
+        message + '\n';
+    messageInputDom.value = '';
+};
+// SEARCH
+document.querySelector('#chat-message-search').onclick = function (e) {
     var messageInputDom = document.querySelector('#chat-message-input');
     var message = messageInputDom.value;
     chatSocket.send(JSON.stringify({
@@ -39,3 +41,4 @@ document.querySelector('#chat-message-submit').onclick = function (e) {
     }));
     messageInputDom.value = '';
 };
+//# sourceMappingURL=domManipulation.js.map
--- static/ts/chat/domManipulation.ts
+++ static/ts/chat/domManipulation.ts
@@ -3,19 +3,16 @@
 // will not be null at runtime.
 
 var wss_protocol = window.location.protocol == 'https:' ? 'wss://' : 'ws://';
-
 const chatSocket = new WebSocket(
     wss_protocol + window.location.host + '/ws/chat/'
 );
 
 // TODO obtain the history
-// ChatSocket behaviour
 chatSocket.onopen = function (e) {
     document.querySelector<HTMLInputElement>('#chat-log')!.value +=
         "Welcome to the noicefluid chat.\nHere you can chat in realtime with all others.\n";
 };
 
-// Append messages
 chatSocket.onmessage = function (e) {
     const data = JSON.parse(e.data);
     var message = data['message'];
@@ -23,31 +20,46 @@ chatSocket.onmessage = function (e) {
         data.message + '\n';
 };
 
-// onclose
 chatSocket.onclose = function (e) {
     document.querySelector<HTMLInputElement>('#chat-log')!.value +=
         'Socket closed unexpectedly, please reload the page.\n';
 };
 
 
-// Submit Button (enter key)
-document.querySelector<HTMLInputElement>('#chat-message-input')!.focus();
 
+document.querySelector<HTMLInputElement>('#chat-message-input')!.focus();
 document.querySelector<HTMLInputElement>('#chat-message-input')!.onkeyup = function (e) {
-    if (e.keyCode === 13) {
-        // enter, return
+    if (e.keyCode === 13) { // enter, return
         document.querySelector<HTMLInputElement>('#chat-message-submit')!.click();
     }
 };
 
-// Submit Button
+// SEND
 document.querySelector<HTMLInputElement>('#chat-message-submit')!.onclick = function (e) {
     const messageInputDom = document.querySelector<HTMLInputElement>('#chat-message-input')!;
     const message = messageInputDom.value;
+
+    document.querySelector<HTMLInputElement>('#chat-log')!.value +=
+        message + '\n';
+
+    messageInputDom.value = '';
+};
+
+
+
+
+
+
+// SEARCH
+document.querySelector<HTMLInputElement>('#chat-message-search')!.onclick = function (e) {
+    const messageInputDom = document.querySelector<HTMLInputElement>('#chat-message-input')!;
+    const message = messageInputDom.value;
+
     chatSocket.send(
         JSON.stringify({
             message: message,
         })
     );
+
     messageInputDom.value = '';
 };
