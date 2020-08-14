f094fe8f58b77f4781b6bc631836aaedb914d06e
drapaiton <drapaiton@gmail.com>
2020-08-14 02:04:11 +0000

    favicon fix ✨

--- noicefluid/urls.py
+++ noicefluid/urls.py
@@ -1,13 +1,17 @@
-# imgs
-from django.conf.urls.static import static
-from django.conf import settings
-# path
 from django.contrib import admin
+
 from django.urls import path
 from django.conf.urls import include
 
+from django.conf.urls.static import static
+from django.conf import settings
+# favicon
+from django.conf.urls import url
+from django.http import HttpResponseRedirect
 urlpatterns = [
     path('', include('lobby.urls')),
-    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+    path('chat/', include('chat.urls')),
+    url(r'^favicon.ico/$',
+        lambda x: HttpResponseRedirect(settings.STATIC_URL + 'favicon.ico')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
