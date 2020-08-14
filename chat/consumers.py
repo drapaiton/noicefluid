d18bbf64ffb12454dbca509e70044028608cb31d
drapaiton <drapaiton@gmail.com>
2020-08-14 00:49:57 +0000

    uncomments 🥱

--- chat/consumers.py
+++ chat/consumers.py
@@ -25,14 +25,11 @@ COMMANDS = {
 
 
 class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
 
-    # TODO protocols
-
-    # def connect(self):
-    #     self.accept()
-
-    # def disconnect(self, close_code):
-    #     pass
+    def disconnect(self, close_code):
+        pass
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
