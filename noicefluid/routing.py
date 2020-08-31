1b6371bd2418eca205d30800be72746ba550c1d3
drapaiton <drapaiton@gmail.com>
2020-08-31 03:14:05 +0000

    Middleware

--- noicefluid/routing.py
+++ noicefluid/routing.py
@@ -1,9 +1,12 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': URLRouter(
-        chat.routing.websocket_urlpatterns
-    ),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
