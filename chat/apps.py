fbb837c890df2ab4c141902eaa33df2d5df316d0
drapaiton <drapaiton@gmail.com>
2020-09-01 14:20:15 +0000

    🤗 yt_api Implementation

--- chat/apps.py
+++ chat/apps.py
@@ -10,7 +10,7 @@ YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 
-class chatConfig(AppConfig):  # Our app config class
+class chatConfig(AppConfig):
     name = "chat"
     YOUTUBE_API = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                         developerKey=DEVELOPER_KEY)
